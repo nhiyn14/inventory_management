c4e951875e816d061beac31d732631e26cf7ce73
declannoble <declanjnoble@hotmail.com>
2022-10-24 16:30:56 +1100

    build: adding env variable

--- front-end/src/AxiosInstance/Instances.js
+++ front-end/src/AxiosInstance/Instances.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const AxiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5000/",
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
         'Authorization': 'Bearer ' + sessionStorage.getItem("token")
     }
